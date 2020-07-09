e23f57bcaf8d07dbc2185f8a75db556f526f71f6
дом <@169.254.170.118>
2020-07-09 19:19:37 +0300

    Сервлет FileUploaderServlet теперь отправляет клиенту выбранный им файл.

--- report.actions/src/report/actions/servlet/FileUploaderServlet.java
+++ report.actions/src/report/actions/servlet/FileUploaderServlet.java
@@ -1,13 +1,18 @@
 package report.actions.servlet;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import report.actions.util.AppUtil;
 
 @WebServlet("/file/upload")
 public class FileUploaderServlet extends HttpServlet 
@@ -22,13 +27,32 @@ public class FileUploaderServlet extends HttpServlet
 	   @Override
 	   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	   {
-	      RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/uploadedFileView.jsp");
-	      dispatcher.forward(request, response);
+		   String fileName = request.getParameter("file");
+		   if (fileName == null || fileName.equals(""))
+		   {
+			   RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/existingFileView.jsp");
+		       dispatcher.forward(request, response);
+		       return;
+		   }
+		   
+	       try (ServletOutputStream out = response.getOutputStream(); 
+	    		InputStream in = new FileInputStream(AppUtil.getReportArchivePath() + "\\" + fileName))
+	       {
+		       byte[] buffer = new byte[1024*64];
+		       int length;
+		       while ((length = in.read(buffer)) > 0)
+		       {
+		    	   out.write(buffer, 0, length);
+		       }
+		       response.setContentType("application/zip");
+		       response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		       out.flush();
+	       }
 	   }
 	 
 	   @Override
 	   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	   {   
-	      doGet(request, response);
+	   {
+		   
 	   }
 }
