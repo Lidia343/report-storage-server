0395af7605a0fa6a26f3c9b0b0c4d8fe1ab3af07
дом <@169.254.170.118>
2020-07-09 15:19:29 +0300

    В сервлете FileGetterServlet реализовано получение архива от клиента по url "http://localhost:8080/report.actions/file". Для этого в методе "doPost" читается входной поток запроса. Сначала из него извлекаются длина имени архива, имя архива и количество вложений, а затем в цикле - длина имени, имя, длина строки размера и размер каждого вложения и содержимое самого вложения. Вложения по мере чтения записываются в выходной поток создаваемого архива. Поле m_lengthOfSizeLine необходимо для задания количества символов, считываемых из входного потока в строку и несущих информацию о длине основных параметров архива (в данный момент значение этой константы равно 2 (скорее всего, в дальнейшем оно не изменится)). Например, длина строки entryCountLine = "02" (количество вложений) равна 2 (клиент добавляет в начало односимвольных строк 0 для удобства чтения потока сервером).

--- report.actions/src/report/actions/servlet/FileGetterServlet.java
+++ report.actions/src/report/actions/servlet/FileGetterServlet.java
@@ -1,6 +1,11 @@
 package report.actions.servlet;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,11 +13,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import report.actions.util.AppUtil;
  
 @WebServlet("/file")
 public class FileGetterServlet extends HttpServlet 
 {
    private static final long serialVersionUID = 1L;
+   private final int m_lengthOfSizeLine = 2;
  
    public FileGetterServlet() 
    {
@@ -27,8 +35,41 @@ public class FileGetterServlet extends HttpServlet
    }
  
    @Override
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, NumberFormatException 
    {
-      doGet(request, response);
+	   InputStream requestIn = request.getInputStream();
+	   
+	   String fileNameLengthLine = AppUtil.getNextStringFromInputStream(requestIn, m_lengthOfSizeLine);
+	   int fileNameLength = Integer.parseInt(fileNameLengthLine);
+	   
+	   String fileName = AppUtil.getNextStringFromInputStream(requestIn, fileNameLength);
+	  
+	   String entryCountLine = AppUtil.getNextStringFromInputStream(requestIn, m_lengthOfSizeLine);
+	   int entryCount = Integer.parseInt(entryCountLine);
+	   
+	   try (BufferedOutputStream bufOut = new BufferedOutputStream(new FileOutputStream(fileName));
+	        ZipOutputStream zout = new ZipOutputStream (bufOut))
+	   {
+		   for(int i = 0 ; i < entryCount; i++)
+		   {
+			   String entryNameLengthLine = AppUtil.getNextStringFromInputStream(requestIn, m_lengthOfSizeLine);
+			   int entryNameLength = Integer.parseInt(entryNameLengthLine);
+			   
+			   String entryName = AppUtil.getNextStringFromInputStream(requestIn, entryNameLength);
+			   zout.putNextEntry(new ZipEntry(entryName));
+			   
+			   String entrySizeLengthLine = AppUtil.getNextStringFromInputStream(requestIn, m_lengthOfSizeLine);
+			   int entrySizeLength = Integer.parseInt(entrySizeLengthLine);
+			   
+			   String entrySizeLine = AppUtil.getNextStringFromInputStream(requestIn, entrySizeLength);
+			   long entrySize = Long.parseLong(entrySizeLine);
+			   
+			   for (long j = 0L; j < entrySize; j++)
+			   {
+				   zout.write(requestIn.read());
+			   }
+			   zout.closeEntry(); 
+		   }
+	   }
    }
 }
