37bc95bcafa3a93ba596efd504b83572ffc6e9fe
дом <@169.254.170.118>
2020-07-12 00:04:59 +0300

    Исправлена обработка запроса получения архива. Теперь, если файлы существуют, клиенту в ответ отправляется страница со списком архивов, каждый из которых можно скачать. Если нет - страница с соответствующим сообщением.

--- report.actions/src/report/actions/servlet/FileGetterServlet.java
+++ report.actions/src/report/actions/servlet/FileGetterServlet.java
@@ -5,10 +5,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,26 +24,63 @@ public class FileGetterServlet extends HttpServlet
    private final int m_lengthOfSizeLine = 2;
    private String m_archivePath = "";
  
-   public FileGetterServlet() throws IOException 
+   public FileGetterServlet () throws IOException 
    {
       super();
       m_archivePath = AppUtil.getReportArchivePath();
    }
    
    @Override
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+   protected void doGet (HttpServletRequest a_request, HttpServletResponse a_response) throws ServletException, IOException 
    {
-	   String jspPath = "/WEB-INF/views/existingFileView.jsp";
 	   File archiveDir = new File(m_archivePath);
-	   if (archiveDir.listFiles().length == 0) jspPath = "/WEB-INF/views/nonExistingFileView.jsp";
-       RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(jspPath);
-       dispatcher.forward(request, response);
+	   File[] files = archiveDir.listFiles();
+	   int fileNum = files.length;
+	   
+	   a_response.setContentType("text/html");  
+	   a_response.setCharacterEncoding("UTF-8");
+	   try(PrintWriter out = a_response.getWriter())
+	   {
+		   out.println("<html>");
+		   if (fileNum != 0) 
+		   {
+			   out.println("\t<head><meta charset='UTF-8'><title>Выбор файлов</title></head>");
+			       
+			   out.println("\t<body>");
+			   out.println("\t\t<jsp:include page='unauthorizationView.jsp'></jsp:include>");
+			   out.println("");
+			   out.println("\t\t<div><p>Выберите файл для загрузки:</p></div><hr>");
+			   out.println("");
+			       
+			   for (File f : files)
+			   {
+				   out.println("\t\t<form action='http://localhost:8080/report.actions/file/upload' method = 'get' enctype = 'multipart/form-data'>");
+			       out.println("\t\t<input type = 'text' name = 'file' value = '" + f.getName() + "' readonly = 'readonly' size = '31'/>");
+			       out.println("\t\t<input type = 'submit' value = 'Загрузить'>");
+			       out.println("\t\t</form>");
+			       out.println("");
+			   }   
+		   }
+		   else
+		   {
+			   out.println("\t<head><meta charset='UTF-8'><title>Файлы не найдены</title></head>");
+		       
+			   out.println("\t<body>");
+			   out.println("\t\t<jsp:include page='unauthorizationView.jsp'></jsp:include>");
+			   out.println("");
+			   out.println("\t\t<div><h1 align = 'center'><em><small>Доступные для загрузки архивы не найдены.</small></em></h1></div><hr>");
+			   out.println("\t\t<div><h1 align = 'center'><em><small><a href = 'file'>Проверить наличие архива для загрузки</a></small></em></h1></div><hr>");
+			   out.println("");
+		   }
+		   out.println("</body>");
+		   out.println("</html>");
+	   }
    }
  
    @Override
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, NumberFormatException 
+   protected void doPost (HttpServletRequest a_request, HttpServletResponse a_response) throws ServletException, IOException, NumberFormatException 
    {
-	   InputStream requestIn = request.getInputStream();
+	   InputStream requestIn = a_request.getInputStream();
 	   
 	   String fileNameLengthLine = AppUtil.getNextStringFromInputStream(requestIn, m_lengthOfSizeLine);
 	   int fileNameLength = Integer.parseInt(fileNameLengthLine);
