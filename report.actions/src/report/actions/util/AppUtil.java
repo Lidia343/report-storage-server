15c55d6681913147fee6262c66d0ad5f9180cc5a
дом <@169.254.170.118>
2020-07-19 07:39:05 +0300

    В класс AppUtil добавлен метод "getStringFromInputStream".

--- report.actions/src/report/actions/util/AppUtil.java
+++ report.actions/src/report/actions/util/AppUtil.java
@@ -43,13 +43,21 @@ public class AppUtil
         if (url != null) return url;
         return null;
     }
- 
-    public static String getNextStringFromInputStream (InputStream a_stream, int a_stringLength) throws IOException
+    
+    public static String getStringFromInputStream (InputStream a_in) throws IOException
+    {
+    	String stringLengthLine = AppUtil.getNextStringFromInputStream(a_in, 2);
+	    int stringLength = Integer.parseInt(stringLengthLine);
+	    
+		return AppUtil.getNextStringFromInputStream(a_in, stringLength);
+    }
+    
+    private static String getNextStringFromInputStream (InputStream a_in, int a_stringLength) throws IOException
     {
     	String result = "";
     	for (int j = 0; j < a_stringLength; j++)
 		{
-    		result += Character.toString((char)a_stream.read());
+    		result += Character.toString((char)a_in.read());
 		}
     	return result;
     }
